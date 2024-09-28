de594ab4f57f959ba2aff6515d674146df52c486
Elvis Liao <llwxi@qq.com>
2024-09-28 18:40:52 +0800

    feat: @formkit/auto-animate

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(autoAnimatePlugin).mount('#app')
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,9 +1,58 @@
 <template>
   <div>
     <h1>GitHub Pages Starter</h1>
-    <p>Pushing code triggers GitHub Actions to be packaged and deployed to GitHub Pages.</p>
+    
+    <ul v-auto-animate>
+      <li v-for="item in list" :key="item">{{ item }}</li>
+    </ul>
+
+    <button @click="handleClick">Add Items</button>
+
+    <div v-auto-animate>
+      <h1>This is a title</h1>
+      <p v-if="isContentShow">This is a very long content. This is a very long content. This is a very long content. This is a very long content. This is a very long content. This is a very long content.</p>
+    </div>
+
+    <button @click="handleContentShowClick">Show Content</button>
   </div>
 </template>
 
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const list = ref([1, 2, 3])
+
+  const handleClick = () => {
+    list.value.push(list.value.length + 1)
+  }
+
+  const isContentShow = ref(false)
+
+  const handleContentShowClick = () => {
+    isContentShow.value = !isContentShow.value
+  }
+</script>
+
+<style scoped>
+  ul {
+    display: flex;
+    gap: 10px;
+    max-width: 50%;
+    flex-wrap: wrap;
+    margin: 0 auto;
+  }
+  li {
+    display: inline-block;
+    border: 1px solid #fff;
+    border-radius: 4px;
+    width: 50px;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+  }
+
+  button {
+    margin-top: 20px;
+  }
+</style>
